78c6b3b6714063125bc12c9b5a60a25c33c338aa
William <william.progdata@gmail.com>
2023-07-21 17:47:15 -0300

    Construindo o degradê do botão

--- app/src/main/java/com/example/teste_dynamox/src/activities/telas/TelaDeLogin.kt
+++ app/src/main/java/com/example/teste_dynamox/src/activities/telas/TelaDeLogin.kt
@@ -3,11 +3,15 @@ package com.example.teste_dynamox.src.activities.telas
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.vector.*
 import androidx.compose.ui.platform.*
@@ -20,6 +24,7 @@ import androidx.compose.ui.modifier.ModifierLocalReadScope
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.example.teste_dynamox.R
+import kotlin.math.round
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -41,10 +46,11 @@ fun TelaDeLogin(context: Context) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Nome de usuário",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 42.dp)
+            Text(
+                text = "Nome de usuário",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 42.dp)
             )
             OutlinedTextField(
                 value = userName,
@@ -62,10 +68,25 @@ fun TelaDeLogin(context: Context) {
             Button(
                 onClick = {
                     //onLoginClick(userName)
-                    Toast.makeText(context, "Botão clicado", Toast.LENGTH_SHORT).show()      },
-                colors = ButtonDefaults.buttonColors(),
+                    Toast.makeText(context, "Botão clicado", Toast.LENGTH_SHORT).show()
+                },
                 contentPadding = PaddingValues(16.dp),
-                modifier = Modifier.fillMaxWidth()
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(MaterialTheme.shapes.extraLarge)
+                    .background(
+                        Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(0xFF76110C),
+                                Color(0xFFCC481A),
+                                Color(0xFFFEC651),
+                            )
+                        )
+                    ),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                )
             ) {
                 Icon(
                     imageVector = ImageVector.vectorResource(id = R.drawable.baseline_login_24),
@@ -74,7 +95,11 @@ fun TelaDeLogin(context: Context) {
                     tint = Color.White
                 )
                 Spacer(modifier = Modifier.width(24.dp))
-                Text("Entrar", color = Color.White , style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+                Text(
+                    "Entrar",
+                    color = Color.White,
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                )
             }
         }
     }
@@ -82,6 +107,6 @@ fun TelaDeLogin(context: Context) {
 
 @Preview(showBackground = true)
 @Composable
-fun TelaDeLoginPreview (){
+fun TelaDeLoginPreview() {
     TelaDeLogin(context = LocalContext.current)
 }
